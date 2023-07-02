269ddcd5fe6bb357bb2bcfc865afeac5bc296268
luisjavierluna <javierlunaluis@gmail.com>
2023-07-02 14:26:01 -0600

    5. Material Sidenav, Toolbar e iconos

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -9,6 +9,7 @@ import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
+import { MaterialModule } from '../material/material.module';
 
 
 
@@ -23,6 +24,7 @@ import { ListadoComponent } from './pages/listado/listado.component';
   imports: [
     CommonModule,
     FlexLayoutModule,
+    MaterialModule,
     HeroesRoutingModule
   ]
 })
