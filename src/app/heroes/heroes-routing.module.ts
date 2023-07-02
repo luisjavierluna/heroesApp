cbfded1c8c9acc5e4ba27718e6553ffcd5fa8649
luisjavierluna <javierlunaluis@gmail.com>
2023-07-01 21:16:24 -0600

    10. Mostrar Rutas Hijas - Segundo RouterOutlet

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -4,10 +4,12 @@ import { ListadoComponent } from './pages/listado/listado.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { AgregarComponent } from './pages/agregar/agregar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const rutas: Routes = [
   {
     path: '',
+    component: HomeComponent,
     children: [
       {path: 'listado', component: ListadoComponent},
       {path: 'agregar', component: AgregarComponent},
