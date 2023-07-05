9cfe29b366091a77dcd0f92f35f56e9421c173ed
luisjavierluna <javierlunaluis@gmail.com>
2023-07-05 17:57:12 -0600

    13. Tarea - PipeImagen

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -11,6 +11,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 import { MaterialModule } from '../material/material.module';
 import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+import { ImagePipe } from './pipes/image.pipe';
 
 
 
@@ -21,7 +22,8 @@ import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.
     HeroeComponent,
     HomeComponent,
     ListadoComponent,
-    HeroeTarjetaComponent
+    HeroeTarjetaComponent,
+    ImagePipe
   ],
   imports: [
     CommonModule,
