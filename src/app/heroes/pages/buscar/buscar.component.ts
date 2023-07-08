93168ae0439b503eee983559915c88211353ec1c
luisjavierluna <javierlunaluis@gmail.com>
2023-07-07 19:03:04 -0600

    19. Autocomplete - Segunda Parte

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Heroe } from '../../interfaces/heroes.interface';
 import { HeroesService } from '../../services/heroes.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-buscar',
@@ -12,6 +13,7 @@ export class BuscarComponent implements OnInit {
 
   termino: string = ''
   heroes: Heroe[] = []
+  heroeSeleccionado!: Heroe
 
   constructor(private heroesService: HeroesService) { }
 
@@ -19,8 +21,16 @@ export class BuscarComponent implements OnInit {
   }
 
   buscando() {
-    this.heroesService.getHeroes()
+    this.heroesService.getSugerencias( this.termino )
       .subscribe( heroes => this.heroes = heroes)
+  }
+
+  opcionSeleccionada(event:MatAutocompleteSelectedEvent) {
+    const heroe: Heroe = event.option.value
+    this.termino = heroe.superhero
+
+    this.heroesService.getHeroePorId(heroe.id!)
+      .subscribe(heroe => this.heroeSeleccionado = heroe)
   }
 
 }
