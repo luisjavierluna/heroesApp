e069520c22c67596282ea67cd634ba30cfb71263
luisjavierluna <javierlunaluis@gmail.com>
2023-07-16 12:36:08 -0600

    8. CanActivate

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -6,21 +6,26 @@ import { AuthService } from '../services/auth.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanLoad {
+export class AuthGuard implements CanLoad, CanActivate {
   
   constructor( private authService: AuthService) { }
 
-  // canActivate(
-  //   route: ActivatedRouteSnapshot,
-  //   state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-  //   return true;
-  // }
+  canActivate( route: ActivatedRouteSnapshot,state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.authService.auth.id) {
+      return true
+    }
+
+    console.log('Bloqueado por el AuthGuard - CanActivate')
+    return false;
+  }
+
   canLoad(route: Route, segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
 
     if (this.authService.auth.id) {
       return true
     }
 
+    console.log('Bloqueado por el AuthGuard - CanLoad')
     return false;
   }
 }
